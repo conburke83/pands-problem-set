3019dd04bcfa6b65f67da757742955f76a6ddcf0
Conor Burke <conburke@gmail.com>
2019-03-14 18:29:25 +0000

    First attempt at problem set 2 on 14/03/2019

---
+++ solution2.py
@@ -0,0 +1,14 @@
+#CB 03/03/2019
+#GMIT Data Analytics Programming & Scripting Module 2019
+#Problem Sets
+#Problem Set 2
+
+#Setting up a variable 't', which stores the value for today's string name
+#Reference https://stackoverflow.com/questions/8380389/how-to-get-day-name-in-datetime-in-python
+import datetime
+t = datetime.datetime.now()
+print ("Does today's day begin with a 'T'?")
+if t[0] == ("T")
+    print ("Yes - today begins with a T.")
+Else print ("No - today does not begin with a T")
+
