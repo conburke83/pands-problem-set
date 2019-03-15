1b9d33f0d5b5a36c46140f58376a716013feb938
Conor Burke <conburke@gmail.com>
2019-03-15 20:16:50 +0000

    First draft of code for solution3. So far, printing list of numbers divisible by 6

--- solution3.py
+++ solution3.py
@@ -2,3 +2,12 @@
 #GMIT Data Analytics Programming & Scripting Module 2019
 #Problem Sets
 #Problem Set 3
+
+#start = 1000
+#End = 10000
+#Count = 
+
+for num in range (1000,10000):
+    if num % 6 == 0:
+        print (num)
+        continue
