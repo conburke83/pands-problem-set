b16c0e3835104d49c17458bceb33de4b2448241a
Conor Burke <conburke@gmail.com>
2019-03-15 20:39:43 +0000

    Final code + comments for solution3 of the problem-sets

--- solution3.py
+++ solution3.py
@@ -3,11 +3,15 @@
 #Problem Sets
 #Problem Set 3
 
-#start = 1000
-#End = 10000
-#Count = 
+#Reference https://www.tutorialspoint.com/python3/nested_if_statements_in_python.htm
 
+#Setting up a controlling loop for variable 'num' for the range 1000 to 10000
 for num in range (1000,10000):
+    #If statement, asking if num divisible by 6
     if num % 6 == 0:
-        print (num)
-        continue
+        #Nested-If statement asking is num not divisible by 12
+         if num % 12 != 0:
+            #If both conditions are satisified, print num, before looping to the next integer in the range
+            print (num)
+            #Even if both conditions are satisfied, continue to the next integer in the range
+            continue
