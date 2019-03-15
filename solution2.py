870f9f63800b4d4d0b99d6e1c7e8ae79ca4ff8a1
Conor Burke <conburke@gmail.com>
2019-03-15 19:41:35 +0000

    Latest version of code for solution 2 in problem set

--- solution2.py
+++ solution2.py
@@ -5,16 +5,21 @@
 
 #Reference https://stackoverflow.com/questions/8380389/how-to-get-day-name-in-datetime-in-python
 #Reference https://pythontic.com/datetime/date/weekday
+#Reference https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date-in-python
 
-#Importing the datetime module
-import datetime
+#Importing the datetime and calendar modules
+from datetime import date
+import calendar
+my_date = date.today()
+calendar.day_name[my_date.weekday()]
 #Setting up a variable 't', which stores the value for today's string name
-t = datetime.datetime.now()
-#Setting up a variable 'tdays' which stores a tuple of weekdays beginning with letter t
-tdays = ("Tuesday","Thursday")
+t = date.today()
+tday = calendar.day_name[t.weekday()]
+#Setting up a variable 't-days' which stores a tuple of weekdays beginning with letter t
+tdays = {"Tuesday","Friday"}
 
 print ("Does today's day begin with a 'T'?")
-if t[0] == "T":
+if tday in tdays:
     print ("Yes - today begins with a T.")
 Else: print ("No - today does not begin with a T")
 
