3a09976b8ba4093a891d058ee29872c84fc77175
Conor Burke <conburke@gmail.com>
2019-03-15 08:55:38 +0000

    Committing latest work on Solution2.py

--- solution2.py
+++ solution2.py
@@ -3,12 +3,18 @@
 #Problem Sets
 #Problem Set 2
 
-#Setting up a variable 't', which stores the value for today's string name
 #Reference https://stackoverflow.com/questions/8380389/how-to-get-day-name-in-datetime-in-python
+#Reference https://pythontic.com/datetime/date/weekday
+
+#Importing the datetime module
 import datetime
+#Setting up a variable 't', which stores the value for today's string name
 t = datetime.datetime.now()
+#Setting up a variable 'tdays' which stores a tuple of weekdays beginning with letter t
+tdays = ("Tuesday","Thursday")
+
 print ("Does today's day begin with a 'T'?")
-if t[0] == ("T")
+if t[0] == "T":
     print ("Yes - today begins with a T.")
-Else print ("No - today does not begin with a T")
+Else: print ("No - today does not begin with a T")
 
