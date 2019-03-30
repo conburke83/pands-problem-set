78fca950a070f783a34037d560b3c61abb243498
Conor Burke <conburke@gmail.com>
2019-03-30 11:03:02 +0000

    Final (final) code + comments for solution 9 of the problem sets. Updated it to include a try/except statement for the user input validation

--- solution9.py
+++ solution9.py
@@ -8,18 +8,22 @@
 #Reference https://stackoverflow.com/questions/38744244/glob-error-io-textiowrapper-name-mode-r-encoding-cp1252-reading-tex
 
 #Creating a user input variable 'userfile', in which the user will enter the name of the user file they would like the program to operate on
-userfile = input("Please enter the name of a txt file: ")
+#Creating this within a 'try-except'statement. If the user enters the name of a valid .txt file the embedded program will run, otherwise the except statement will just re-prompt the user to enter a valid filename.
+try:
+    userfile = input("Please enter the name of a txt file: ")
 
-#Open the text file 'solution9_text in read mode and give it the object name 'f'.
-#Opening via a 'with' statement to perform actions on the file, and also to close the txt file automatically at the end of the 'with' statement
-with open(userfile,'r') as f:
-    #Create a variable 'count' which will keep a count of the lin number incrementation, starting at zero
-    linenumber = 0
-    #Creating a loop for printing individual lines of the text file
-    for line in f:
-        #For each iteration of the loop increment the'linenumber' by 1
-        linenumber+=1
-        #Nested if-statement asking if the linenumber is now an odd number. If yes, print that line, if no return to the beginning of the loop
-        if linenumber % 2 == 0:
-            print(line)
-
+    #Open the text file 'solution9_text in read mode and give it the object name 'f'.
+    #Opening via a 'with' statement to perform actions on the file, and also to close the txt file automatically at the end of the 'with' statement
+    with open(userfile,'r') as f:
+        #Create a variable 'count' which will keep a count of the lin number incrementation, starting at zero
+        linenumber = 0
+        #Creating a loop for printing individual lines of the text file
+        for line in f:
+            #For each iteration of the loop increment the'linenumber' by 1
+            linenumber+=1
+            #Nested if-statement asking if the linenumber is now an odd number. If yes, print that line, if no return to the beginning of the loop
+            if linenumber % 2 == 0:
+                print(line)
+#Except statement to reprompt the user to enter a valid filename   
+except:
+    userfile = input("Please enter the name of a txt file: ")
